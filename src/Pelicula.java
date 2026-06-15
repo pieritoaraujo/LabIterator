423b8ab0412d4b3dca2e1734f7df6cabe1ebb247
Piero Araujo <pieritoeeuu@gmail.com>
2026-06-14 20:43:48 -0500

    feat: implementar clase Pelicula

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,2 +1,35 @@
 public class Pelicula {
+    private String titulo;
+    private String genero;
+    private double ranking;
+
+    public Pelicula(String titulo, String genero, double ranking) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.ranking = ranking;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public double getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(double ranking) {
+        this.ranking = ranking;
+    }
 }
